16e3488507dd429ffdb4943d42461865112fbb48
Vítězslav Imrýšek <vitezslav.imrysek@outlook.cz>
2023-10-02 22:39:29 +0200

    Improvements

--- WinStalker/MainWindow.xaml.cs
+++ WinStalker/MainWindow.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
+
+using Control = System.Windows.Forms.Control;
+using Keys = System.Windows.Forms.Keys;
+using Point = System.Drawing.Point;
+using PInvoke = Windows.Win32.PInvoke;
 
 namespace WinStalker
 {
@@ -12,50 +16,92 @@ namespace WinStalker
     /// </summary>
     public partial class MainWindow : Window
     {
+        private CancellationTokenSource _cts;
+
         public MainWindow()
         {
             InitializeComponent();
-
+            
             Loaded += OnLoaded;
         }
 
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
+            RunMouseLoop();
+        }
+
+        private void RunMouseLoop()
+        {
+            _cts = new CancellationTokenSource();
+
             var dispatcher = Dispatcher;
             var dpi = VisualTreeHelper.GetDpi(this);
 
-            var callback = new DispatcherOperationCallback(OnSet);
+            Task.Run(() => OnMouseLoop(dispatcher, dpi, _cts.Token));
+        }
 
-            Task.Run(async () =>
+        private async Task OnMouseLoop(Dispatcher dispatcher, DpiScale dpi, CancellationToken token)
+        {
+            const int DelaySleep = 16;
+            const int DelayStandBy = 2;
+            const int DelayKeyboard = 16;
+            const int SleepTicks = 500 / DelayStandBy; // 500 ms divided by time for one tick
+
+            var sleepTicks = SleepTicks;
+            var mousePos = Point.Empty;
+
+            var data = new WindowsPosition();
+            var callback = new DispatcherOperationCallback(WindowPositionCallback);
+
+            var manualReset = new ManualResetEvent(false);
+
+            while (true)
             {
-                var data = new Data();
-
-                while (true)
+                if (token.IsCancellationRequested)
                 {
-                    var modifiers = System.Windows.Forms.Control.ModifierKeys;
-                    if (((modifiers & System.Windows.Forms.Keys.Control) == System.Windows.Forms.Keys.Control)
-                        && ((modifiers & System.Windows.Forms.Keys.Shift) == System.Windows.Forms.Keys.Shift))
+                    return;
+                }
+
+                var modifiers = Control.ModifierKeys;
+                if (((modifiers & Keys.Control) == Keys.Control) && ((modifiers & Keys.Shift) == Keys.Shift))
+                {
+                    sleepTicks = SleepTicks;
+                    await Task.Delay(DelayKeyboard);
+                }
+                else
+                {
+                    PInvoke.GetCursorPos(out var mousePosNew);
+                    if (mousePosNew == mousePos)
                     {
-                        await Task.Delay(2);
+                        if (sleepTicks > 0)
+                        {
+                            sleepTicks--;
+
+                            // Wait actively, Task.Delay has resolution of about 10-16ms.
+                            manualReset.WaitOne(DelayStandBy);
+                        }
+                        else
+                        {
+                            await Task.Delay(DelaySleep);
+                        }
                     }
                     else
                     {
-                        var screenPosition = System.Windows.Forms.Control.MousePosition;
+                        sleepTicks = SleepTicks;
+                        mousePos = mousePosNew;
 
-                        data.X = (int)((screenPosition.X + 20) / dpi.DpiScaleX);
-                        data.Y = (int)((screenPosition.Y + 20) / dpi.DpiScaleY);
+                        data.X = (int)((mousePos.X + 20) / dpi.DpiScaleX);
+                        data.Y = (int)((mousePos.Y + 20) / dpi.DpiScaleY);
 
                         await dispatcher.BeginInvoke(DispatcherPriority.Normal, callback, data);
-
-                        await Task.Delay(1);
                     }
                 }
-            });
+            }
         }
 
-        private object OnSet(object arg)
+        private object WindowPositionCallback(object arg)
         {
-            var data = arg as Data;
+            var data = arg as WindowsPosition;
 
             Left = data.X;
             Top = data.Y;
@@ -63,10 +109,10 @@ namespace WinStalker
             return null;
         }
 
-        private class Data
+        private class WindowsPosition
         {
-            public int X;
-            public int Y;
+            public int X { get; set; }
+            public int Y { get; set; }
         }
     }
 }
